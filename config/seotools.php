fbc72f59536fac05d8bf0989560263f730e879d0
abraham ghaemi <ig.ghaemi@gmail.com>
2021-05-31 13:30:39 +0430

    update mata

--- config/seotools.php
+++ config/seotools.php
@@ -9,9 +9,10 @@ return [
          * The default configurations to be used by the meta generator.
          */
         'defaults'       => [
-            'title'        => "It's Over 9000!", // set false to total remove
+            'title'        => "صبا سلز | نرم افزار مدیریت ارتباط با مشتری (سی آر ام) صبا", // set false to total remove
             'titleBefore'  => false, // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
-            'description'  => 'For those who helped create the Genki Dama', // set false to total remove
+            'description'  => 'نرم افزار CRM صبا سلز |   ۱۰۰ درصدر رضایت مشتری  | راه ایده آل برای فروش بیشتر
+صبا به شما این امکان را میدهد تا تمام اطلاعات مشتریان خود را در یک مکان جمع آوری کرده و معاملات خود را پیگری کنید.', 
             'separator'    => ' - ',
             'keywords'     => [],
             'canonical'    => false, // Set null for using Url::current(), set false to total remove
@@ -58,9 +59,10 @@ return [
          * The default configurations to be used by the json-ld generator.
          */
         'defaults' => [
-            'title'       => 'Over 9000 Thousand!', // set false to total remove
-            'description' => 'For those who helped create the Genki Dama', // set false to total remove
-            'url'         => false, // Set null for using Url::current(), set false to total remove
+            'title'        => "صبا سلز | نرم افزار مدیریت ارتباط با مشتری (سی آر ام) صبا", // set false to total remove
+                        'description'  => 'نرم افزار CRM صبا سلز |   ۱۰۰ درصدر رضایت مشتری  | راه ایده آل برای فروش بیشتر
+صبا به شما این امکان را میدهد تا تمام اطلاعات مشتریان خود را در یک مکان جمع آوری کرده و معاملات خود را پیگری کنید.', 
+            'url'         => null, // Set null for using Url::current(), set false to total remove
             'type'        => 'WebPage',
             'images'      => [],
         ],
